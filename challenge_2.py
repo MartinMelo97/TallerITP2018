e5b12c682b2654aa1fa643e75bb7a52de5256b66
Martin Melo <nitram-210397@hotmail.com>
2018-10-08 12:19:41 -0500

    Falta solo un inciso para terminar el challenge2

--- challenge_2.py
+++ challenge_2.py
@@ -15,7 +15,7 @@ frase = input("Escribe la frase para este challenge #2: ")
 
 #Creamos una lista la cual contiene cada una de las palabras de la frase, separándolas con split
 lista_frase = frase.split(" ")
-
+print(lista_frase)
 #Una vez teniendo la lista, utilicemos "len" para contar el numero de elementos que tiene la lista
 #Que vendrían siendo el número de palabras en la frase
 no_palabras = len(lista_frase)
@@ -24,5 +24,34 @@ no_palabras = len(lista_frase)
 #La primer palabra, tendríamos qu acceder al primer elemento de la lista, que tiene un indice 0
 primer_palabra = lista_frase[0]
 
+#Ahora mostraremos la frase desde la tercer palabra
+#método "join" permite poder pasar una lista en un string, utilizando el caracter puesto antes de escribir join
+#Tambien recordemos que con los breads podemos cortar nuestros strings o listas desde un determinado punto
+#En este caso colocaremos [2:] para omitir las 2 primera palabras, mostrando desde la 3ra hasta el fin de la frase
+tercer_palabra = lista_frase[2:]
+separador = " "
+tercer_palabra_en_string = separador.join(tercer_palabra)
+print(tercer_palabra_en_string)
 
+#Para añadir la palabra ITPTICS, utilizaremos el método "append" que nos permite añadir nuevos elementos
+#a una lista.
+#Nuevamente utilizaremos el método "join" para mostrarlo en forma de texto y no de lista
 
+lista_frase.append("ITPTICS")
+print(separador.join(lista_frase))
+
+#Invertir la frase es muy sencllo, sólo debemos utilizar el método "reverse" para las listas
+lista_frase.reverse()
+print(separador.join(lista_frase))
+
+#Ahora quitaremos la última palabra de la lista original, es decir, sin contar la palabra ITPTICS
+#Y sin haber invertido la frase, entonces... Pensemos.
+#Con la frase invertida, en la lista la palabra ITPTICS pasó de ser el último elemento de la lista al primero
+#Por lo tanto tiene un índice 0. Entonces la palabra que estaba antes de ITPTICS (la que nos interesa)
+#Al invertir la frase quedó como segundo elemento de la lista, teniendo como índice 1
+#Entonces, sabiendo ya en que lugar de la lista quedó la última palabra de la lista origina, basta con
+#utilizar el método del
+ultima_palabra_original = lista_frase[1]
+print("La palabra que ya no debe aparecer abajo es: ", ultima_palabra_original)
+del lista_frase[1]
+print(lista_frase)
