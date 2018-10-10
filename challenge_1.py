c22a0b16fe2454002c010ea8cc8ab4c3958e06ab
Martin Melo <nitram-210397@hotmail.com>
2018-10-09 23:19:17 -0500

    Ampliamos challenge 1

--- challenge_1.py
+++ challenge_1.py
@@ -1,35 +1,42 @@
 # Challenge #1 Taller de Python en el ITP (10-Octubre-2018)
 
-#Recibir 2 numeros e imprimir:
-#Suma ("La suma es:")
-#Resta
-#Multiplicacion
-#División
-#División Entera
-#Suma de cuadrados
+# Recibir 2 numeros e imprimir:
+# Suma ("La suma es:")
+# Resta
+# Multiplicacion
+# División
+# División Entera
+# Suma de cuadrados
+# Suma de raíces
 
-#CODE
+# CODE
 
-#Recibimos los 2 números
-#Recordemos que al utilizar "Input" lo que reciba del teclado será de tipo stringo
-#Por lo que ese string que recibimos lo parseamos a tipo entero o flotante, en este caso, entero.
+#Importamos la librería math para utilizar sqrt
+import math
+
+# Recibimos los 2 números
+# Recordemos que al utilizar "Input" lo que reciba del teclado será de tipo string
+# Por lo que ese string que recibimos lo parseamos a tipo entero o flotante, en este caso, entero.
+
 
 num1 = int(input("Escribe el primer número: "))
 num2 = int(input("Escribe el segundo número "))
 
-#Comenzamos a realizar las operaciones solicitadas, almacenando cada una en una variable diferente
+# Comenzamos a realizar las operaciones solicitadas, almacenando
+# cada una en una variable diferente
 suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
 division = num1 / num2
-division_entera = num1 //num2
+division_entera = num1 // num2
 modulo = num1 % num2
 suma_cuadrados = num1**2 + num2**2
+suma_raices = math.sqrt(num1) + math.sqrt(num2)
 
-#Mostramos los resultados, concatenando la leyenda indicada en el challenge con el valor
-#Recordemos que para concatenar, podemos usar "+" o ","
-#La diferencia es que el operador "+" solo funciona con variables tipo string
-#Mientras que el operador "," con cualquier tipo de dato, y agrega un espacio automáticamente
+# Mostramos los resultados, concatenando la leyenda indicada en el challenge con el valor
+# Recordemos que para concatenar, podemos usar "+" o ","
+# La diferencia es que el operador "+" solo funciona con variables tipo string
+# Mientras que el operador "," con cualquier tipo de dato, y agrega un espacio automáticamente
 print("---------------------------------")
 print()
 print("La suma es: " + str(suma))
@@ -39,3 +46,4 @@ print("La division es:", division)
 print("La division entera es:", division_entera)
 print("El módulo es:", modulo)
 print("La suma de cuadrados es:", suma_cuadrados)
+print("La suma de raíces es:", suma_raices)
---
+++ escribir.py
@@ -0,0 +1,8 @@
+
+input("Introduce un valor...")
+
+int("2") = 2
+
+str(35) = "35"
+
+len("palabra") = 7
